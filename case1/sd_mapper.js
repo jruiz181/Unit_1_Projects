e5e359cfbdadeb0592326f3f8cc9bb93d430c372
Jeremiah RUiz <Jruiz181@west-mec.org>
2020-01-16 14:37:35 -0700

    finished

--- case1/sd_mapper.js
+++ case1/sd_mapper.js
@@ -8,12 +8,18 @@
    Date:   
 
 */
+// makes javascript not work if theres a problem
 "use strict"
+//  creates a new variable
 var thisTime = new Date
 var timeStr = thisTime.toLocaleString();
+// gets timestamp id from html to equal date variable;
+
 document.getElementById("timeStamp").innerHTML= timeStr;
+// gets hours and months from the date
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
+// fromula to plug in new images every so often
 var mapNum = (2 * thisMonth + thisHour) % 24;
 var imgStr = "<img src='sd_sky" + mapNum + ".png'/>";
 document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr)
